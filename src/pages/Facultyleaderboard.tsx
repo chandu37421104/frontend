a0c7e9686be07a400a074b5e384e3555d141e9cc
chandu37421104 <144286107+chandu37421104@users.noreply.github.com>
2024-12-05 18:22:57 -0500

    Update Facultyleaderboard.tsx

--- src/pages/Facultyleaderboard.tsx
+++ src/pages/Facultyleaderboard.tsx
@@ -1,4 +1,4 @@
-/import React, { useEffect, useState } from 'react';
+/*import React, { useEffect, useState } from 'react';*/
 import { getUsers } from '../services/userService';
 
 export const Facultyleaderboard = () => {
