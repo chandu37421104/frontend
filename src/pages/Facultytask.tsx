945bcd41c38fc75e133cb04a1c702bbf5dddb259
chandu37421104 <144286107+chandu37421104@users.noreply.github.com>
2024-12-05 18:24:56 -0500

    Update Facultytask.tsx

--- src/pages/Facultytask.tsx
+++ src/pages/Facultytask.tsx
@@ -1,4 +1,4 @@
-/*import React, { useState, useEffect } from 'react';*/
+import React, { useState, useEffect } from 'react';
 import { getTasks, updateTask } from '../services/taskService';
 import { Task } from '../types';
 
