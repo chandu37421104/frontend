2797877931c34ae5caa42e37ed5004280ff40d78
chandu37421104 <144286107+chandu37421104@users.noreply.github.com>
2024-12-05 18:22:42 -0500

    Update Facultyrewards.tsx

--- src/pages/Facultyrewards.tsx
+++ src/pages/Facultyrewards.tsx
@@ -1,4 +1,4 @@
-/import React, { useEffect, useState } from 'react';
+/*import React, { useEffect, useState } from 'react';*/
 import { getTasks } from '../services/taskService';
 
 export const Facultyrewards = () => {
